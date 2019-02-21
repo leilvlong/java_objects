d104fc8774bb1add753c7a10ef7addc9a7d1b4d9
leilvlong <1455013965@qq.com>
2019-02-21 09:41:06 +0800

    破碎代码补充输出

---
+++ object_basis/EchoTestDrive.java
@@ -0,0 +1,42 @@
+public class EchoTestDrive{
+	public static void main(String[] args){
+		EchoTest e1 = new EchoTest();
+		EchoTest e2 = new EchoTest();
+		int x = 0;
+		while (x<4){
+			e1.hello();
+			e1.cont += 1;
+			System.out.println("e1 cont:" + e1.cont);
+			if (x == 3){
+				e2.cont += 1;
+				System.out.println("if x == 3 e2.cont:" + e2.cont);
+			}
+			if (x > 0){
+				System.out.println("if x > 0");
+				e2.cont = e2.cont + e1.cont;
+				System.out.println("e1 + e2"+ e2.cont);
+			}
+			System.out.println("x:" + x);
+			x++;
+		}
+		System.out.println("Ñ­»·½áÊøºóe2.cont:"+e2.cont);
+		
+	}
+}
+
+
+/*
+x = 0
+e1.cont = 1
+e2.cont = 1
+
+x = 1
+e1.cont = 2
+e2.cont = 2
+*/
+class EchoTest{
+	int cont = 0;
+	void hello(){
+		System.out.println("helloooo....");
+	}
+}
